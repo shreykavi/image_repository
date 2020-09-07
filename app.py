2379e53c9dd9c1eb0184e2bc140dcd840ac1029d
Shrey <shrey.kavi@gmail.com>
2020-09-07 16:31:22 -0400

    Upload and download jpg from server

--- app.py
+++ app.py
@@ -1,22 +1,58 @@
 
-import os 
+import os
 
-from flask import Flask
+from flask import Flask, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from os.path import join, dirname, realpath, abspath
+# from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'uploads/')
+ALLOWED_EXTENSIONS = ['png', 'jpg']
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #Max size = 16 MB
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
 
 from models import Result
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/upload/image', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # filename = secure_filename(file.filename)
+            filename = "test.jpg"
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return "Hello World!"
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 @app.route('/test', methods=['POST'])
 def test():
--- models.py
+++ models.py
@@ -17,3 +17,19 @@ class Result(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+# class Photo(db.Model):
+#     __tablename__ = 'photo'
+
+#     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+#     url = db.Column(db.String())
+#     # result_all = db.Column(JSON)
+#     # result_no_stop_words = db.Column(JSON)
+
+#     def __init__(self, url, result_all, result_no_stop_words):
+#         self.url = url
+#         # self.result_all = result_all
+#         # self.result_no_stop_words = result_no_stop_words
+
+#     def __repr__(self):
+#         return '<id {}>'.format(self.id)
